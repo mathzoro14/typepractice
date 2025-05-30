689250c9c997bbe3982e2388e1d88f1e7392c74a
mathzoro14 <gongja614@gmail.com>
2025-05-31 00:04:16 +0900

    edit

--- KEYPRACT/KEYPRACT/show_string.c
+++ KEYPRACT/KEYPRACT/show_string.c
@@ -8,7 +8,7 @@ void show_string(char *p,int stage) {
 	}
 	else
 	{
-		for (i = 0;i < SIZE + stage * 2;i++) // 2단계 이상에서는 대소문자로 하기
+		for (i = 0;i < SIZE + stage * 2;i++) //2단계 이상에서 대소문자로
 		{
 			if(rand()%2==0)
 				p[i] = 'a' + rand() % 26;
